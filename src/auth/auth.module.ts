9dafc96d045c15981e5094ac8b2c65dc963c525d
Esteban <estebanespg1@gmail.com>
2026-08-30 21:00:56 -0500

    jwt constant fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,15 +3,18 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_CONSTANT'),
+        signOptions: { expiresIn: '1h' },
+      }),
       global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
