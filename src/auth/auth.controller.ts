5cd930fe336915830e89bda3d3c694979613a1e7
Esteban <estebanespg1@gmail.com>
2026-08-31 21:46:45 -0500

    add swagger documentation for auth endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { LoginUserDto } from 'src/users/dto/login-user.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -10,12 +11,28 @@ export class AuthController {
 
   @Post('signup')
   @Public()
+  @ApiOperation({ summary: 'Register new user' })
+	@ApiResponse({
+		status: 201,
+		description: 'User successfully registered',
+	})
+	@ApiResponse({ status: 400, description: 'Bad Request' })
+	@ApiResponse({
+		status: 409,
+		description: 'Conflict - Username already exists',
+	})
   signUp(@Body() createUserDto: CreateUserDto) {
     return this.authService.signUp(createUserDto);
   }
 
   @Post('signin')
   @Public()
+  @ApiOperation({ summary: 'Authenticate as user' })
+	@ApiResponse({
+		status: 200,
+		description: 'Login successful',
+	})
+	@ApiResponse({ status: 401, description: 'Unauthorized' })
   signIn(@Body() loginUserDto: LoginUserDto) {
     return this.authService.signIn(loginUserDto);
   }
--- src/users/dto/login-user.dto.ts
+++ src/users/dto/login-user.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class LoginUserDto {
+  @ApiProperty()
   @IsEmail()
   @IsNotEmpty()
   email!: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(8)
   password!: string;
